2ae52c6c035531cddd1e5ea23e2f4f560bfec824
giuseppericcio <giu.riccio@gmail.com>
2023-11-13 09:10:44 +0100

    fixed some responsive errors and finished the menu and the registration form

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//Make mobile navigation work
+const btnNav = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNav.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
